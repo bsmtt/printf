8f1383029bd963e4c754286171f29c2d5d8ff433
basma ebraheim <basmaebraheim@gmail.com>
2023-10-18 01:56:59 +0300

    print string

---
+++ print_strings.c
@@ -0,0 +1,108 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * print_char - writes the char from args to stdout
+ * @args: args to get next arg char
+ * @flags: flags for format string
+ * Return: 1
+ */
+int print_char(va_list *args, flag_t *flags)
+{
+char str = va_arg(*args, int);
+
+(void) flags;
+
+_write_buffer(str);
+return (1);
+}
+/**
+ * print_percent - writes % to stdout
+ * @args: args
+ * @flags: flags for format string
+ * Return: 1
+ */
+int print_percent(va_list *args, flag_t *flags)
+{
+(void) args;
+(void) flags;
+
+_write_buffer('%');
+return (1);
+}
+/**
+ * print_string - writes string to stdout
+ * @args: args
+ * @flags: flags for format string
+ * Return: number of printed characters
+ */
+int print_string(va_list *args, flag_t *flags)
+{
+char *s = va_arg(*args, char *);
+int i = 0;
+int precision = flags->precision;
+
+if (s == NULL)
+{
+s = "(null)";
+}
+if (precision)
+{
+while (*s && i < precision)
+{
+_write_buffer(*s);
+s++;
+i++;
+}
+}
+else
+{
+while (*s)
+{
+_write_buffer(*s);
+s++;
+i++;
+}
+}
+return (i);
+}
+/**
+ * print_ustring - prints a string
+ * @args: args to get next arg char*
+ * @flags: flags for format string
+ * Return: number of printed chargcters
+ */
+int print_ustring(va_list *args, flag_t *flags)
+{
+unsigned char *str;
+char *hex;
+int i = 0, length = 0;
+
+str = va_arg(*args, unsigned char *);
+
+while (str[i])
+{
+if (str[i] < 32 || str[i] >= 127)
+{
+_write_buffer('\\');
+_write_buffer('x');
+
+flags->upperHex = 87;
+hex = converter(str[i], 20, 16, flags);
+if (!hex)
+return (-1);
+/*print last two digits in hex*/
+_write_buffer(hex[14]);
+_write_buffer(hex[15]);
+free(hex);
+length += 4;
+}
+else
+{
+_write_buffer(str[i]);
+length++;
+}
+i++;
+}
+return (length);
+}
