ee3cc11309fc35976f0898bacc6311df2efac374
basma ebraheim <basmaebraheim@gmail.com>
2023-10-18 01:58:55 +0300

    main

---
+++ main.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+int len;
+int len2;
+unsigned int ui;
+void *addr;
+len = _printf("%5d\n", 123);
+
+
+
+return (0);
+}
