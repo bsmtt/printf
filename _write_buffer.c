041432877137846af29b8e6c394b95a7a185031c
bsmtt <basmatalaat224@gmail.com>
2023-10-17 15:28:58 -0700

    write buffer

---
+++ _write_buffer.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _write_buffer - write character
+ * @c: charcter to concatenate
+ * Return: void
+ */
+void _write_buffer(char c)
+{
+	static char buffer[1024];
+	static int bufferCount;
+
+	/* reset buffer if get to 1024*/
+	if (bufferCount == 1024 || !c)
+	{
+		write(1, buffer, bufferCount);
+		bufferCount = 0;
+	}
+
+	buffer[bufferCount] = c;
+	bufferCount = bufferCount + 1;
+}
