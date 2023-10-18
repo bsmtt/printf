7b5d4428741d71c457cb012d3d0657580eecce9f
basma ebraheim <basmaebraheim@gmail.com>
2023-10-18 22:45:49 +0300

    update

--- _printf.c
+++ _printf.c
@@ -27,7 +27,7 @@ while (set_flags(c + i, &flags))
 i++;
 if(!c[i] || c[i] == '\n')
 {
-_write_buffer('%'), length++;
+_write_buffer('%'), _write_buffer('\n'), length = length + 2;
 goto endprint;
 }
 startIndex = i;
